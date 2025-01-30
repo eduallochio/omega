abc76de7f9398b5963d7c88a75e3c6630067e1b7
eduallochio.github.io <43136553+eduallochio@users.noreply.github.com>
2025-01-30 17:34:14 -0300

    feat: add authentication whith clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,15 @@
 import Image from 'next/image';
 import { Button } from '../_components/ui/button';
 import { LogInIcon } from 'lucide-react';
+import { SignInButton } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-const LoginPage = () => {
+const LoginPage = async () => {
+	const { userId } = await auth();
+	if (userId) {
+		redirect("/");
+	}
 	return (
 		<div className="grid h-full grid-cols-2 ">
 			{/*esquerda */}
@@ -10,10 +17,12 @@ const LoginPage = () => {
 				<Image src="/logo.svg" width={173} height={39} alt='Omega Finance' className='mb-8' />
 				<h1 className='text-4xl font-bold mb-3'>Bem-vindo</h1>
 				<p className='text-muted-foreground mb-8'>O Ômega Finance é uma plataforma de gestão financeira que utiliza IA para monitorar suas movimentações, ele oferece insights personalizados, facilitando o controle de seu orçamento</p>
-				<Button variant="outline">
-					<LogInIcon className='mr-2' />
-					Fazer login ou criar conta
-				</Button>
+				<SignInButton>
+					<Button variant="outline">
+						<LogInIcon className='mr-2' />
+						Fazer login ou criar conta
+					</Button>
+				</SignInButton>
 			</div>
 			{/*direita */}
 			<div className='relative h-full w-full'>
@@ -30,3 +39,4 @@ const LoginPage = () => {
 }
 
 export default LoginPage;
+
